f47ce046a831a95bc46a8c45545c9d08a8e3d4c1
Jacob Wiltse <aquabug@gmail.com>
2019-04-02 21:31:40 -0400

    add readme, assign roles

---
+++ README.md
@@ -0,0 +1,2 @@
+# Discord as Code
+This is an experimental project to have a community run Discord using pull requests and approvals for deciding and server changes.
--- main.tf
+++ main.tf
@@ -66,11 +66,18 @@ resource "discord_channel_invite" "test_invite" {
   max_age = 0
 }
 
-//resource "discord_guild_member" "kelwing" {
-//  guild_id = "${discord_guild.dsac.id}"
-//  member_id = "109710323094683648"
-//  roles = ["${discord_guild_role.admin.id}"]
-//}
+resource "discord_guild_member" "kelwing" {
+  guild_id = "${discord_guild.dsac.id}"
+  member_id = "109710323094683648"
+  roles = ["${discord_guild_role.admin.id}"]
+}
+
+resource "discord_guild_member" "trophias" {
+  guild_id = "${discord_guild.dsac.id}"
+  member_id = "85185381837836288"
+  roles = ["${discord_guild_role.moderator.id}"]
+}
+
 
 output "invite" {
   value = "${discord_channel_invite.test_invite.code}"

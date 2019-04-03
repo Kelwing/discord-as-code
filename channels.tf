c09056dd2db82b7eaf647ec69b33cbd7ff927369
Jacob Wiltse <aquabug@gmail.com>
2019-04-03 00:18:11 -0400

    didn't actuall need to touch this

--- channels.tf
+++ channels.tf
@@ -28,7 +28,7 @@ resource "discord_guild_channel" "announce" {
   guild_id = "${discord_guild.dsac.id}"
   name = "announcements"
   parent_id = "${discord_guild_channel.announcements.id}"
-  position = 3
+  position = 1
 }
 
 
